7b8fc6f696f2771021e9c6a74e1210545a8b54ff
Amin <price1375@yahoo.com>
2023-03-10 16:50:25 +0330

    delete bootstrap import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 import Header from "./Header/Main/Header";
